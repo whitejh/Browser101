12b8b9f2ac4fccca031f100ebebaec4d33da43e4
whitejh <kei02012@naver.com>
2021-02-18 16:04:11 +0900

    좌표실습성능개선

--- coordinates/main.js
+++ coordinates/main.js
@@ -3,19 +3,21 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', () => {
-  const x = event.clientX;
-  const y = event.clientY;
-  console.log(`${x} ${y}`);
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`;
+  document.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
 
-  horizontal.style.top = `${y}px`;
-
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
-  tag.innerHTML = `${x}px, ${y}px`; // 초기 Hello에 left, top값을 전달
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+    tag.innerHTML = `${x}px, ${y}px`; // 초기 Hello에 left, top값을 전달
+  });
 });
